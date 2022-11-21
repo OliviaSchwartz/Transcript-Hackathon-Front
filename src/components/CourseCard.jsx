d4e783ca2fadee1c9880048dabedb63d950e51bf
Olivia Schwartz <ocmooney@gmail.com>
2022-11-21 16:38:54 -0500

    courses to update

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,10 +1,11 @@
-import {useState} from 'react'
+import Courses from "../pages/Courses"
 
 const CourseCard = (props)=> {
     return(
-        <div className= "card course-card">
+        <div className= "card course-card" >
             <div className= "course-info">
-                <h3 className="name-display">{props.name}</h3>
+                <h3 className="name-display">Course: {props.name}</h3>
+                <button>Join this class</button>
             </div>
         </div>
     )
--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,8 +1,32 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { navigate, useNavigate } from 'react-router-dom'
+import { GetCourses } from '../services/CourseServices'
+import CourseCard from '../components/CourseCard'
 
 const Courses = ({ user, authenticated }) => {
-  return <div>List of courses</div>
+  let navigate = useNavigate()
+  const [courses, setCourses] = useState([])
+
+  const showAllCourses = async () => {
+    const data = await GetCourses()
+    setCourses(data)
+  }
+
+  useEffect(() => {
+    showAllCourses()
+  }, [courses])
+
+  const viewCourse = (_id) => {
+    navigate(`${_id}`)
+  }
+
+  return (
+    <div className="course-container">
+      {courses.map((course) => (
+        <CourseCard key={course.id} name={course.name} courseId={course.id} />
+      ))}
+    </div>
+  )
 }
 
 export default Courses
