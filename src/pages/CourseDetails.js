7607cc75f04a306149cf458b36d17ef38ce8baab
Amaya Lucas <amayajlucas@gmail.com>
2022-11-22 10:30:02 -0500

    edit grades button visibility

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -8,11 +8,11 @@ const CourseDetails = ({ user, authenticated }) => {
 
   const initialState = {
     courseId: id,
-    // studentId: null,
     grade: ''
   }
   const [formState, setFormState] = useState(initialState)
   const [students, setStudents] = useState(null)
+  const [studentToEdit, setStudentToEdit] = useState()
 
   const handleChange = (e) => {
     setFormState({ ...formState, [e.target.id]: e.target.value })
@@ -27,7 +27,13 @@ const CourseDetails = ({ user, authenticated }) => {
     e.preventDefault()
     await UpdateGrade({ ...formState, studentId: id })
     setFormState(initialState)
+    setStudentToEdit(null)
     getStudentsInCourse()
+  }
+
+  const onClick = (id) => {
+    console.log(id)
+    setStudentToEdit(id)
   }
 
   useEffect(() => {
@@ -42,20 +48,27 @@ const CourseDetails = ({ user, authenticated }) => {
         <div key={student.id}>
           <p>{student.name}</p>
           <p>{student.Grade.grade}</p>
-          <form className="form" onSubmit={(e) => handleSubmit(e, student.id)}>
-            <input
-              className="input"
-              type="text"
-              id="grade"
-              placeholder="New Grade in 4 Point Scale"
-              onChange={(e) => handleChange(e)}
-              value={formState.grade}
-              // required
-            />
-            <button className="create-course-button" type="submit">
-              Update Grade
-            </button>
-          </form>
+          {studentToEdit === student.id ? (
+            <form
+              className="form"
+              onSubmit={(e) => handleSubmit(e, student.id)}
+            >
+              <input
+                className="input"
+                type="text"
+                id="grade"
+                placeholder="New Grade in 4 Point Scale"
+                onChange={(e) => handleChange(e)}
+                value={formState.grade}
+                // required
+              />
+              <button className="create-course-button" type="submit">
+                Update Grade
+              </button>
+            </form>
+          ) : (
+            <button onClick={() => onClick(student.id)}>Edit</button>
+          )}
         </div>
       ))}
     </div>
