8995d46e1d74fd8f924c1d7d9a30121f36d753b5
Amaya Lucas <amayajlucas@gmail.com>
2022-11-22 10:20:03 -0500

    update grade progress

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -1,7 +1,63 @@
+import { useState, useEffect } from 'react'
+import { GetCoursesById } from '../services/CourseServices'
+import { useParams } from 'react-router-dom'
+import { UpdateGrade } from '../services/GradeServices'
+
 const CourseDetails = ({ user, authenticated }) => {
+  let { id } = useParams()
+
+  const initialState = {
+    courseId: id,
+    // studentId: null,
+    grade: ''
+  }
+  const [formState, setFormState] = useState(initialState)
+  const [students, setStudents] = useState(null)
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value })
+  }
+
+  const getStudentsInCourse = async (req, res) => {
+    const studentsInCourse = await GetCoursesById(id)
+    setStudents(studentsInCourse)
+  }
+
+  const handleSubmit = async (e, id) => {
+    e.preventDefault()
+    await UpdateGrade({ ...formState, studentId: id })
+    setFormState(initialState)
+    getStudentsInCourse()
+  }
+
+  useEffect(() => {
+    getStudentsInCourse()
+  }, [])
+
   return (
     <div>
       <p>course details</p>
+      <p>{students?.name}</p>
+      {students?.students.map((student) => (
+        <div key={student.id}>
+          <p>{student.name}</p>
+          <p>{student.Grade.grade}</p>
+          <form className="form" onSubmit={(e) => handleSubmit(e, student.id)}>
+            <input
+              className="input"
+              type="text"
+              id="grade"
+              placeholder="New Grade in 4 Point Scale"
+              onChange={(e) => handleChange(e)}
+              value={formState.grade}
+              // required
+            />
+            <button className="create-course-button" type="submit">
+              Update Grade
+            </button>
+          </form>
+        </div>
+      ))}
     </div>
   )
 }
--- src/services/GradeServices.js
+++ src/services/GradeServices.js
@@ -1,8 +1,22 @@
 import Client from './api'
 
-export const GetGrade = async ({courseId, studentId}) => {
+export const GetGrade = async ({ courseId, studentId }) => {
   try {
     const res = await Client.get(`/grades/${courseId}/${studentId}`)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const UpdateGrade = async ({ courseId, studentId, grade }) => {
+  try {
+    console.log(`courseId ${courseId}`)
+    console.log(`studentId ${studentId}`)
+    console.log(`grade ${grade}`)
+    const res = await Client.put(`/grades/${courseId}/${studentId}`, {
+      grade: grade
+    })
     return res.data
   } catch (error) {
     throw error
